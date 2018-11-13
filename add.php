9e4b5e67eb9b2f43a38f7bf1d014fe4e295c1c20
Michael Salvati <print@3daddict.com>
2018-11-13 10:31:47 -0800

    addBook data created

--- add.php
+++ add.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/inc/nav.php';
 <div class="container">
     <div class="well">
         <h2>Add a book</h2>
-        <form action="procedures/addBook.php" method="post" class="form-horizontal">
+        <form action="./procedures/addBook.php" method="post" class="form-horizontal">
             <?php include __DIR__ . '/inc/bookform.php'; ?>
         </form>
     </div>
--- procedures/addBook.php
+++ procedures/addBook.php
@@ -3,3 +3,18 @@ require_once __DIR__ . '../inc/bootstrap.php';
 
 $bookTitle = request()->get('title');
 $bookDescription = request()->get('description');
+
+try {
+    $newBook = addBook($bookTitle, $bookDescription);
+    $response = \Symfony\Component\HttpFoundation\Response::create(null, 
+    \Symfony\Component\HttpFoundation\Response::HTTP_FOUND, 
+    ['Location' => '/books.php']);
+    $response->send();
+    exit;
+} catch (\Exception $e) {
+    $response = \Symfony\Component\HttpFoundation\Response::create(null, 
+    \Symfony\Component\HttpFoundation\Response::HTTP_FOUND, 
+    ['Location' => '/add.php']);
+    $response->send();
+    exit;
+}
